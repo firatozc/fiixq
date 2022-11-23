50edb1944f0a41b645e7b926fc7727c32196369b
firatozc <115105034+firatozc@users.noreply.github.com>
2022-11-23 21:20:28 +0300

    Update POLİNDROM NUMBER.py

--- POLİNDROM NUMBER.py
+++ POLİNDROM NUMBER.py
@@ -5,15 +5,6 @@ Created on Thu Oct 20 16:47:13 2022
 @author: User
 """
 
-#Number=int(input("please enter the number: "))
- 
-#if Number%10==Number //100:
- #   print(Number,"sayısı polindrom sayıdır.")                     ##### 3 basamaklı sayılar içindir.
-    
-#else:
- #   print(Number,"sayısı polindrom sayı değildir.")
- 
-
 Number=int(input("please enter the number: "))
 temp=Number
 reversenumber=0
